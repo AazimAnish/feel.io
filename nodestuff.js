d2125fd158ac52c8b9facce116d2607f8a785a14
Mishal <mishal0404@gmail.com>
2023-11-05 03:13:39 +0530

    kasjhdashd

--- nodestuff.js
+++ nodestuff.js
@@ -23,6 +23,10 @@ app.post('/normalmoods', (req, res) => {
   runj().then(x=>{res.send(x)})
 });
 
+app.post('/nthhour', (req, res) => {
+  runb(req.body.hour).then(x=>{res.send(x)})
+});
+
 app.post('/journalentry', (req, res) => {
   runp(req.body).then(x=>{res.send(x)})
 });
@@ -139,15 +143,78 @@ async function runj() {
   const aggregation = [
     {
       $match: {
+        mood: {
+          $in: ["joy", "anger", "neutral", "sadness"]
+        },
         timestamp: {
           $gte: (Date.now() / 1000) - 3600 * 24 * 30  // Current time in seconds minus 3600 seconds (1 hour)
         }
       }
     },
     {
+      $group: {
+        _id: "$mood",
+        count: { $sum: 1 }
+      }
+    },
+    {
+      $project: {
+        _id: 0,
+        mood: "$_id",
+        count: 1
+      }
+    },
+    {
+      $group: {
+        _id: null,
+        moodCounts: {
+          $push: {
+            k: "$mood",
+            v: "$count"
+          }
+        }
+      }
+    },
+    {
+      $replaceRoot: {
+        newRoot: {
+          $arrayToObject: "$moodCounts"
+        }
+      }
+    }
+  ]
+  const result2 = await feelio.aggregate(aggregation).toArray();
+  console.log(result2)
+  return result2
+}
+
+
+
+
+async function runb(hour) {
+  const uri = "mongodb+srv://mishal0404:mishal2003@mishal0404.35lsnon.mongodb.net/?retryWrites=true&w=majority";
+  
+  const client = new MongoClient(uri, {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    }
+  });
+
+  await client.connect();
+  const database = client.db("feelio");
+  const feelio = database.collection("feelio");
+  
+  const aggregation = [
+    {
       $match: {
         mood: {
           $in: ["joy", "anger", "neutral", "sadness"]
+        },
+        timestamp: {
+          $gte: (Date.now() / 1000) - 3600 * (hour+2), // Start of the 7th hour
+          $lt: (Date.now() / 1000) - 3600 * (hour+1)  // End of the 6th hour
         }
       }
     },
